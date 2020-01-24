35ab44a70d286aeee34353b2d22037e72627bb9c
yuhwanjun <48980540+yuhwanjun@users.noreply.github.com>
2020-01-24 18:35:42 +0900

    first

---
+++ js/draggable.js
@@ -0,0 +1,193 @@
+$(function () {
+    $(".draggable").draggable({
+        stack: ".draggable"
+    });
+});
+
+
+//toggle 1
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_1').click(function () {
+            $('#poster_1').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_1').click(function () {
+            $('#poster_1').fadeToggle();
+        });
+    });
+});
+
+//toggle 2
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_2').click(function () {
+            $('#poster_2').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_2').click(function () {
+            $('#poster_2').fadeToggle();
+        });
+    });
+});
+
+//toggle 3
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_3').click(function () {
+            $('#poster_3').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_3').click(function () {
+            $('#poster_3').fadeToggle();
+        });
+    });
+});
+
+//toggle 4
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_4').click(function () {
+            $('#poster_4').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_4').click(function () {
+            $('#poster_4').fadeToggle();
+        });
+    });
+});
+
+//toggle 5
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_5').click(function () {
+            $('#poster_5').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_5').click(function () {
+            $('#poster_5').fadeToggle();
+        });
+    });
+});
+
+//toggle 6
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_6').click(function () {
+            $('#poster_6').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_6').click(function () {
+            $('#poster_6').fadeToggle();
+        });
+    });
+});
+
+//toggle 7
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_7').click(function () {
+            $('#poster_7').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_7').click(function () {
+            $('#poster_7').fadeToggle();
+        });
+    });
+});
+
+//toggle 8
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_8').click(function () {
+            $('#poster_8').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_8').click(function () {
+            $('#poster_8').fadeToggle();
+        });
+    });
+});
+
+//toggle 9_1
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_9_1').click(function () {
+            $('#poster_9_1').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_9').click(function () {
+            $('#poster_9_1').fadeToggle();
+        });
+    });
+});
+
+//toggle 9_2
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_9_2').click(function () {
+            $('#poster_9_2').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_9').click(function () {
+            $('#poster_9_2').fadeToggle();
+        });
+    });
+});
+
+//toggle 10
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_10').click(function () {
+            $('#poster_10').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_10').click(function () {
+            $('#poster_10').fadeToggle();
+        });
+    });
+});
+
+//toggle 10
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_11').click(function () {
+            $('#poster_11').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_11').click(function () {
+            $('#poster_11').fadeToggle();
+        });
+    });
+});
+
+
+
+
+
+
+
