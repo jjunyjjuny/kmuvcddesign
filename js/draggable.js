718937f9bdf62cfaf1f795c814be9a3c7f3d6092
yuhwanjun <48980540+yuhwanjun@users.noreply.github.com>
2020-02-06 21:04:02 +0900

    1

---
+++ js/draggable.js
@@ -0,0 +1,211 @@
+$(function () {
+    $(".draggable").draggable({
+        stack: ".draggable",
+        handle: "div.draggable_box"
+    });
+});
+
+
+//toggle 1
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_1').click(function () {
+            $('#poster_1').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_1').click(function () {
+            $('#poster_1').fadeToggle();
+            $('#poster_1').css('z-index', '16');
+        });
+    });
+});
+
+//toggle 2
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_2').click(function () {
+            $('#poster_2').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_2').click(function () {
+            $('#poster_2').fadeToggle();
+            $('#poster_2').css('z-index', '16');
+        });
+    });
+});
+
+//toggle 3
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_3').click(function () {
+            $('#poster_3').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_3').click(function () {
+            $('#poster_3').fadeToggle();
+            $('#poster_3').css('z-index', '16');
+        });
+    });
+});
+
+//toggle 4
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_4').click(function () {
+            $('#poster_4').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_4').click(function () {
+            $('#poster_4').fadeToggle();
+            $('#poster_4').css('z-index', '16');
+        });
+    });
+});
+
+//toggle 5
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_5').click(function () {
+            $('#poster_5').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_5').click(function () {
+            $('#poster_5').fadeToggle();
+            $('#poster_5').css('z-index', '16');
+        });
+    });
+});
+
+//toggle 6
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_6').click(function () {
+            $('#poster_6').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_6').click(function () {
+            $('#poster_6').fadeToggle();
+            $('#poster_6').css('z-index', '16');
+        });
+    });
+});
+
+//toggle 7
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_7').click(function () {
+            $('#poster_7').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_7').click(function () {
+            $('#poster_7').fadeToggle();
+            $('#poster_7').css('z-index', '16');
+        });
+    });
+});
+
+//toggle 8
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_8').click(function () {
+            $('#poster_8').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_8').click(function () {
+            $('#poster_8').fadeToggle();
+            $('#poster_8').css('z-index', '16');
+        });
+    });
+});
+
+//toggle 9_1
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_9_1').click(function () {
+            $('#poster_9_1').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_9').click(function () {
+            $('#poster_9_1').fadeToggle();
+            $('#poster_9_1').css('z-index', '16');
+        });
+    });
+});
+
+//toggle 9_2
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_9_2').click(function () {
+            $('#poster_9_2').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_9').click(function () {
+            $('#poster_9_2').fadeToggle();
+            $('#poster_9_2').css('z-index', '16');
+        });
+    });
+});
+
+//toggle 10
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_10').click(function () {
+            $('#poster_10').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_10').click(function () {
+            $('#poster_10').fadeToggle();
+            $('#poster_10').css('z-index', '16');
+        });
+    });
+});
+
+//toggle 11
+$(document).ready(function () {
+    $(function () {
+        $('.close_icon_11').click(function () {
+            $('#poster_11').fadeToggle();
+        });
+    });
+
+    $(function () {
+        $('.main_icon_11').click(function () {
+            $('#poster_11').fadeToggle();
+            $('#poster_11').css('z-index', '16');
+        });
+    });
+});
+
+
+// 클릭시 가장 앞으로
+
+$(document).ready(function () {
+    $(function () {
+        $('.draggable').click(function () {
+            $(this).css('z-index', '16');
+        });
+    });
+});
+
